078bae83d86fa0d31495ad3bee2a155052da6e74
Arbab Riffat <riffatroxx@gmail.com>
2026-06-23 18:25:32 -0400

    Day 01: Variables and Data Types

--- 01_CPP_BASICS/01_hello_world.cpp
+++ 01_CPP_BASICS/01_hello_world.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main () {
     cout << "Hello Arbab";
-    cout << "I am preparing for TCS NAT 19th July exam";
+    cout << "I am preparing for TCS NAT 19th July exam\n";
     cout << "i will crack Placements";
     return 0;
 }
---
+++ 01_CPP_BASICS/02_variables.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int age = 21;
+    float height = 5.8;
+    double salary = 50000.50;
+    char grade = 'A';
+    bool isPlaced = false;
+
+    cout << "Age: " << age << endl;
+    cout << "height : " << height << endl;
+    cout << "salary: " << salary << endl;
+    cout << "Grade: " << grade << endl ;
+    cout << " isPlaced: " << isPlaced << endl;
+
+    return 0;
+
+}
